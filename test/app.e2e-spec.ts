4c94804495f60f59b548cf6b76293ca2ce084eea
Tamer Ağaoğlu <tamer.a@90pixel.com>
2022-11-11 17:49:10 +0300

    updated jest config

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,7 +1,8 @@
 import * as request from 'supertest';
 import { Test } from '@nestjs/testing';
-import { AppModule } from './../src/app.module';
-import { INestApplication } from '@nestjs/common';
+import { AppModule } from '../src/app.module';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { FastifyAdapter } from '@nestjs/platform-fastify';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -11,14 +12,23 @@ describe('AppController (e2e)', () => {
       imports: [AppModule],
     }).compile();
 
-    app = moduleFixture.createNestApplication();
+    app = moduleFixture.createNestApplication(new FastifyAdapter());
+    app.useGlobalPipes(new ValidationPipe({ transform: true }));
     await app.init();
+    app.getHttpAdapter().getInstance().ready();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  it('api document', async () => {
+    const apiRequest = await request(app.getHttpServer())
+      .post('/auth/register')
+      .send({
+        username: 'username',
+        password: 'test12345',
+      });
+    expect(apiRequest.status).toBe(201);
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
 });
