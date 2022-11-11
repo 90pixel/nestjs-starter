983e8f9372a9cb1c858d765a62f825024e797b07
Tamer Ağaoğlu <tamer.a@90pixel.com>
2022-11-11 14:42:26 +0300

    convert Express to FastifyAdapter nest app

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+
   app.setGlobalPrefix('api');
 
   //validation class
